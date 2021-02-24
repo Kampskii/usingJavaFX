7c5ea94f9c37520865adf21acbbece92ed2476ec
Kampskii <aklot22@scienceleadership.org>
2021-02-24 10:32:50 -0500

    Added Images and in the process of adding them in

--- src/org/sla/UsingJavaFx.java
+++ src/org/sla/UsingJavaFx.java
@@ -3,53 +3,45 @@ package org.sla;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
-import javax.swing.text.html.ImageView;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 
 public class UsingJavaFx extends Application {
     // Fields for UI elements
-    Label someText;
-    Label moreText;
-    Button firstButton;
+    Image TinykeanuImage; Image TinysteveImage; Image guacardoImage;
+    ImageView Tinykeanu; ImageView Tinysteve; ImageView Guacardo;
+    FileInputStream input;
 
-    public void userClicked(){
-        moreText.setText("Stan Erwin Smith");
-    }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) throws FileNotFoundException {
         stage.setTitle("Cevans");
 
-        // creating UI elements
-        firstButton = new Button("click click");
-        firstButton.setOnAction(actionEvent -> {
-            userClicked();
-        });
-        someText = new Label("helloooo");
-        moreText = new Label("Stan Reiner");
+        // creating UI elements ??
 
-        //Importing Image
-        FileInputStream input = new FileInputStream("");
-        Image image = new image(input);
-        ImageView imageView = new ImageView(input);
+        // Importing Image(s)
 
-        //Check box(s)
-        CheckBox checkBox1 = new CheckBox("Choice 1");
-        CheckBox checkBox2 = new CheckBox("Choice 2");
+        input = new FileInputStream("/Users/alyciaklot/Downloads/Tiny Keanu.png");
+        TinykeanuImage = new Image(input);
+        Tinykeanu = new ImageView(TinykeanuImage);
+        Tinykeanu.setFitHeight(189); Tinykeanu.setFitWidth(150);
 
-        checkBox1.setOnAction(actionEvent -> {
-            System.out.println("Blue Pill");
-        });
+        input = new FileInputStream("/Users/alyciaklot/Downloads/Tiny Steve Rogers.png");
+        TinysteveImage = new Image(input);
+        Tinysteve = new ImageView(TinysteveImage);
+        Tinysteve.setFitHeight(190); Tinysteve.setFitWidth(190);
 
-        checkBox2.setOnAction(actionEvent -> {
-            System.out.println("Red Pill");
-        });
+        //input = new FileInputStream("/Users/alyciaklot/Downloads/Guacardo.png");
+        //guacardoImage = new Image(input);
+        //Guacardo = new ImageView(guacardoImage);
 
-        // Radio Button(s)
+
+            // Radio Button(s)
         RadioButton radioButton1 = new RadioButton("1");
         RadioButton radioButton2 = new RadioButton("2");
         RadioButton radioButton3 = new RadioButton("3");
@@ -59,25 +51,36 @@ public class UsingJavaFx extends Application {
         radioButton2.setToggleGroup(radioGroup);
         radioButton3.setToggleGroup(radioGroup);
 
+            // Radio Button(s) actions
         radioButton1.setOnAction(actionEvent -> {
             System.out.println("I am the right brain");
         });
-
         radioButton2.setOnAction(actionEvent -> {
             System.out.println("I am the left brain");
         });
-
         radioButton3.setOnAction(actionEvent -> {
             System.out.println("And I'm just here");
         });
 
+        // Check box(s)
+        CheckBox checkBox1 = new CheckBox("Choice 1");
+        CheckBox checkBox2 = new CheckBox("Choice 2");
+
+        // Check box(s) actions
+        checkBox1.setOnAction(actionEvent -> {
+            System.out.println("Blue Pill");
+        });
+        checkBox2.setOnAction(actionEvent -> {
+            System.out.println("Red Pill");
+        });
+
+
         // creating Layout
-        HBox thyLayout = new HBox(firstButton, someText, moreText, radioButton1, radioButton2,radioButton3,checkBox1,checkBox2);
+        HBox thyLayout = new HBox(Tinykeanu);
 
         // creating Scene
         Scene thyScene = new Scene(thyLayout);
         stage.setScene(thyScene);
-
         stage.setWidth(500);
         stage.setHeight(500);
 
