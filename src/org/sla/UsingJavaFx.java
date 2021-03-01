f8df1b6a05fe450f8dce89abda1c75c595d8d5cb
Kampskii <aklot22@scienceleadership.org>
2021-03-01 14:06:51 -0500

    Imported Images (All five)

--- src/org/sla/UsingJavaFx.java
+++ src/org/sla/UsingJavaFx.java
@@ -13,19 +13,17 @@ import java.io.FileNotFoundException;
 
 public class UsingJavaFx extends Application {
     // Fields for UI elements
-    Image TinykeanuImage;
-    Image TinysteveImage;
-    Image guacardoImage;
-
-    ImageView Tinykeanu;
-    ImageView Tinysteve;
-    ImageView Guacardo;
+    Image TinykeanuImage;  ImageView Tinykeanu;
+    Image TinysteveImage; ImageView Tinysteve;
+    Image guacardoImage; ImageView Guacardo;
+    Image dodgerImage; ImageView Dodger;
+    Image leviImage; ImageView Levi;
 
     FileInputStream input1; //Keanu
     FileInputStream input2; //Steve
     FileInputStream input3; //Guacardo
-    FileInputStream input4;
-    FileInputStream input5;
+    FileInputStream input4; //Dodger
+    FileInputStream input5; //Levi
 
 
     @Override
@@ -35,7 +33,6 @@ public class UsingJavaFx extends Application {
         // creating UI elements ??
 
         // Importing Image(s)
-
         input1 = new FileInputStream("/Users/alyciaklot/Downloads/Tiny Keanu.png");
         TinykeanuImage = new Image(input1);
         Tinykeanu = new ImageView(TinykeanuImage);
@@ -49,52 +46,28 @@ public class UsingJavaFx extends Application {
         input3 = new FileInputStream("/Users/alyciaklot/Downloads/Guacardo.png");
         guacardoImage = new Image(input3);
         Guacardo = new ImageView(guacardoImage);
-        Guacardo.setFitHeight(267); Guacardo.setFitWidth(200);
+        Guacardo.setFitHeight(191); Guacardo.setFitWidth(143);
 
+        input4 = new FileInputStream("/Users/alyciaklot/Downloads/Dodger.jpg");
+        dodgerImage = new Image(input4);
+        Dodger = new ImageView(dodgerImage);
+        Dodger.setFitHeight(190); Dodger.setFitWidth(153);
 
-            // Radio Button(s)
-        RadioButton radioButton1 = new RadioButton("1");
-        RadioButton radioButton2 = new RadioButton("2");
-        RadioButton radioButton3 = new RadioButton("3");
+        input5 = new FileInputStream("/Users/alyciaklot/Downloads/Freaked Out Levi.jpg");
+        leviImage = new Image(input5);
+        Levi = new ImageView(leviImage);
+        Levi.setFitHeight(190); Levi.setFitWidth(233);
 
-        ToggleGroup radioGroup = new ToggleGroup();
-        radioButton1.setToggleGroup(radioGroup);
-        radioButton2.setToggleGroup(radioGroup);
-        radioButton3.setToggleGroup(radioGroup);
+        //Thy Buttons/Menus/idk
 
-            // Radio Button(s) actions
-        radioButton1.setOnAction(actionEvent -> {
-            System.out.println("I am the right brain");
-        });
-        radioButton2.setOnAction(actionEvent -> {
-            System.out.println("I am the left brain");
-        });
-        radioButton3.setOnAction(actionEvent -> {
-            System.out.println("And I'm just here");
-        });
-
-        // Check box(s)
-        CheckBox checkBox1 = new CheckBox("Choice 1");
-        CheckBox checkBox2 = new CheckBox("Choice 2");
-
-        // Check box(s) actions
-        //checkBox1.setOnAction(actionEvent -> );
-        checkBox2.setOnAction(actionEvent -> {
-            System.out.println("Red Pill");
-        });
-
-        //public static void userClicked1() {
-            //ImageView.set //}
 
         // creating Layout
-        HBox thyLayout = new HBox(Tinykeanu, Tinysteve, Guacardo);
-
+        HBox thyLayout = new HBox(Tinykeanu);
         // creating Scene
         Scene thyScene = new Scene(thyLayout);
         stage.setScene(thyScene);
         stage.setWidth(500);
         stage.setHeight(500);
-
         // display Scene
         stage.show();
     }
