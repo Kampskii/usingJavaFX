e21656c46e69452e34923ba70cef99145831cd6a
Kampskii <aklot22@scienceleadership.org>
2021-03-10 14:54:32 -0500

    Broken again

--- src/org/sla/UsingJavaFx.java
+++ src/org/sla/UsingJavaFx.java
@@ -12,81 +12,118 @@ import java.io.FileNotFoundException;
 
 
 public class UsingJavaFx extends Application {
-    // Fields for UI elements
-    Image TinykeanuImage;  ImageView Tinykeanu;
-    Image TinysteveImage; ImageView Tinysteve;
-    Image guacardoImage; ImageView Guacardo;
-    Image dodgerImage; ImageView Dodger;
-    Image leviImage; ImageView Levi;
+    // Fields
+    Image TinykeanuImage;
+    Image TinysteveImage;
+    Image reinerImage;
+    Image leviImage;
+    Image erwinImage;
 
-    ImageView imageView;
+    ImageView imageView1;
+    ImageView imageView2;
+    ImageView imageView3;
+    ImageView imageView4;
 
     FileInputStream input1; //Keanu
     FileInputStream input2; //Steve
-    FileInputStream input3; //Guacardo
-    FileInputStream input4; //Dodger
-    FileInputStream input5; //Levi
+    FileInputStream input3; //Reiner
+    FileInputStream input4; //Levi
+    FileInputStream input5; //Erwin
 
+    ToggleButton toggleButton1;
+
+    MenuButton menuButton;
+    MenuItem menuItem1;
+    MenuItem menuItem2;
+    MenuItem menuItem3;
 
     @Override
     public void start(Stage stage) throws FileNotFoundException {
-        stage.setTitle("Cevans");
+        stage.setTitle("Test 1");
 
         // creating UI elements ??
+        imageView1 = new ImageView();
+        imageView2 = new ImageView();
+        imageView3 = new ImageView();
+        imageView4 = new ImageView();
 
         // Importing Image(s)
 
         input1 = new FileInputStream("/Users/alyciaklot/Downloads/Tiny Keanu.png");
         TinykeanuImage = new Image(input1);
-        Tinykeanu = new ImageView(TinykeanuImage);
-        Tinykeanu.setFitHeight(189); Tinykeanu.setFitWidth(150);
 
         input2 = new FileInputStream("/Users/alyciaklot/Downloads/Tiny Steve Rogers.jpg");
         TinysteveImage = new Image(input2);
-        Tinysteve = new ImageView(TinysteveImage);
-        Tinysteve.setFitHeight(190); Tinysteve.setFitWidth(190);
 
-        input3 = new FileInputStream("/Users/alyciaklot/Downloads/Guacardo.png");
-        guacardoImage = new Image(input3);
-        Guacardo = new ImageView(guacardoImage);
-        Guacardo.setFitHeight(191); Guacardo.setFitWidth(143);
+        input3 = new FileInputStream("/Users/alyciaklot/Downloads/Reiner.png");
+        reinerImage = new Image(input3);
 
-        input4 = new FileInputStream("/Users/alyciaklot/Downloads/Dodger.jpg");
-        dodgerImage = new Image(input4);
-        Dodger = new ImageView(dodgerImage);
-        Dodger.setFitHeight(190); Dodger.setFitWidth(153);
+        input4 = new FileInputStream("/Users/alyciaklot/Downloads/Freaked Out Levi.jpg");
+        leviImage = new Image(input4);
 
-        input5 = new FileInputStream("/Users/alyciaklot/Downloads/Freaked Out Levi.jpg");
-        leviImage = new Image(input5);
-        Levi = new ImageView(leviImage);
-        Levi.setFitHeight(190); Levi.setFitWidth(233);
+        input5 = new FileInputStream("/Users/alyciaklot/Downloads/Erwin.jpg");
+        erwinImage = new Image(input5);
 
         //Thy Buttons/Menus/idk
 
         //Toggle Button(s)
-        ToggleButton toggleButton1 = new ToggleButton("Choice 1");
+        toggleButton1 = new ToggleButton("Idk Press it");
         toggleButton1.setOnAction(actionEvent -> userClicked1() );
-        ToggleButton toggleButton2 = new ToggleButton("Choice 2");
 
+        final ToggleGroup group = new ToggleGroup();
+        group.getToggles().addAll(toggleButton1);
 
-
+        //Menu Button
+        menuButton = new MenuButton("AOT", imageView1, menuItem1, menuItem2, menuItem3);
+        menuItem1 = new MenuItem("Erwin Smith");
+        menuItem1.setOnAction(actionEvent -> userClicked2() );
+        menuItem2 = new MenuItem("Levi Ackerman");
+        menuItem2.setOnAction(actionEvent -> userClicked3() );
+        menuItem3 = new MenuItem("Reiner Braun");
+        menuItem3.setOnAction(actionEvent -> userClicked4() );
 
         // creating Layout
-        HBox thyLayout = new HBox(toggleButton1, toggleButton2);
+        HBox thyLayout = new HBox(toggleButton1, menuButton, imageView1, imageView2, imageView3, imageView4);
         // creating Scene
         Scene thyScene = new Scene(thyLayout);
-        stage.setScene(thyScene);
-        stage.setWidth(500);
-        stage.setHeight(500);
         // display Scene
         stage.show();
+        stage.setScene(thyScene);
+        stage.setWidth(800);
+        stage.setHeight(800);
+    }
+
+    private void userClicked1() { //Have it switch to Steve
+        boolean isSelected = toggleButton1.isSelected();
+        if (isSelected) {
+            imageView1.setImage(TinykeanuImage);
+            imageView1.setFitHeight(252);
+            imageView1.setFitWidth(200);
+        } else {
+            imageView1.setImage(TinysteveImage);
+            imageView1.setFitHeight(249);
+            imageView1.setFitWidth(250);
+
+        }
+    }
+
+    private void userClicked2() {
+        imageView2.setImage(erwinImage);
+        imageView2.setFitHeight(251);  imageView2.setFitWidth(476);
+    }
+
+    private void userClicked3() {
+        imageView3.setImage(leviImage);
+        imageView3.setFitHeight(2);  imageView3.setFitWidth(310);
+    }
+
+    private void userClicked4() {
+        imageView4.setImage(reinerImage);
+        imageView4.setFitHeight(252);  imageView4.setFitWidth(336);
     }
 
     public static void main(String[] args) {
         Application.launch(args);
     }
 
-    private void userClicked1() {
-        Tinykeanu.setImage(TinykeanuImage);
-    }
 }
